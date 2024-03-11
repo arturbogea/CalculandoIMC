fd82f4452eba388dfc73842bdb1c1c3c1d1a7ef5
Artur Bogéa <arturbogea@gmail.com>
2024-03-11 20:33:18 -0300

    Criando a tela splash.

--- app/src/main/java/com/arturbogea/calculandoimc/SplashActivity.kt
+++ app/src/main/java/com/arturbogea/calculandoimc/SplashActivity.kt
@@ -1,11 +1,27 @@
 package com.arturbogea.calculandoimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.arturbogea.calculandoimc.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivitySplashBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4200)
+
     }
 }
