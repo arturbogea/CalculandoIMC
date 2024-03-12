ad7415b37efb3162272424274b6a2766c1aade79
Artur Bogéa <arturbogea@gmail.com>
2024-03-12 08:53:30 -0300

    Atualização do app. Subindo os fragments de resultado e calculo.

--- app/src/main/java/com/arturbogea/calculandoimc/fragments/CalculoIMC.kt
+++ app/src/main/java/com/arturbogea/calculandoimc/fragments/CalculoIMC.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import com.arturbogea.calculandoimc.R
+import com.arturbogea.calculandoimc.databinding.ActivitySplashBinding
 import com.arturbogea.calculandoimc.databinding.FragmentCalculoBinding
 
 class CalculoIMC : Fragment() {
 
     private var binding: FragmentCalculoBinding? = null
 
+    private var pesoFinal: Double? = null
+    private var alturaFinal: Double? = null
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        pesoFinal = arguments?.getDouble("$pesoFinal")
+        alturaFinal = arguments?.getDouble("$alturaFinal")
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,8 +33,26 @@ class CalculoIMC : Fragment() {
         val view = binding!!.root
 
 
+        binding!!.btnCalcular.setOnClickListener {
+            val nome = binding!!.editNome.text.toString()
+            val peso = binding!!.editPeso.text.toString().toDoubleOrNull() ?: 0.0
+            val altura = binding!!.editAltura.text.toString().toDoubleOrNull() ?: 0.0
+
+            val bundle = bundleOf("peso" to peso, "altura" to altura)
+            val resultadoImc = ResultadoIMC()
+            resultadoImc.arguments = bundle
+
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_conteudo, resultadoImc)
+                .addToBackStack(null)
+                .commit()
+        }
+
+
 
         return view
     }
 
 }
+
+
--- app/src/main/java/com/arturbogea/calculandoimc/fragments/ResultadoIMC.kt
+++ app/src/main/java/com/arturbogea/calculandoimc/fragments/ResultadoIMC.kt
@@ -1,4 +1,99 @@
 package com.arturbogea.calculandoimc.fragments
 
-class ResultadoIMC {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.arturbogea.calculandoimc.R
+import com.arturbogea.calculandoimc.databinding.FragmentCalculoBinding
+import com.arturbogea.calculandoimc.databinding.FragmentResultadoBinding
+
+class ResultadoIMC : Fragment() {
+
+    private var binding: FragmentResultadoBinding? = null
+    private var pesoFinal: Double = 0.0
+    private val alturaFinal: Double = 0.0
+
+    val bundle = arguments
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val pesoFinal = arguments?.getDouble("peso") ?: 0.0
+        val alturaFinal = arguments?.getDouble("altura") ?: 0.0
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentResultadoBinding.inflate(inflater, container, false)
+        val view = binding!!.root
+
+        resultadoFinal()
+
+        binding!!.btnVoltar.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+
+        return view
+    }
+
+    fun resultadoFinal(){
+
+        val imagem = binding!!.imgResul
+        val resultadoFinal = binding!!.txtResul
+        val txtPeso = binding!!.txtPeso
+        val txtAltura = binding!!.txtAltura
+
+
+        var peso = pesoFinal
+        var altura = alturaFinal
+        var imc = peso / (altura * altura)
+
+        imc.toString()
+
+        txtPeso.text = "O peso informado foi de $pesoFinal"
+        txtAltura.text = "A altura informada foi de $alturaFinal"
+
+
+        when {
+            imc <= 18.5 -> {
+                resultadoFinal.setText("O seu imc é de ${"%.2f".format(imc)}. Você está abaixo do peso")
+                resultadoFinal.setTextColor(requireContext().getColor(R.color.orange))
+                imagem.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.alerta))
+
+            }
+            imc <= 24.9 -> {
+                resultadoFinal.setText("O seu imc é de ${"%.2f".format(imc)}. Você está com o peso normal")
+                resultadoFinal.setTextColor(requireContext().getColor(R.color.darkGreen))
+                imagem.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ok))
+            }
+            imc <= 29.9 -> {
+                resultadoFinal.setText("O seu imc é de ${"%.2f".format(imc)}. Você está acima do peso")
+                resultadoFinal.setTextColor(requireContext().getColor(R.color.yellow))
+                imagem.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.alerta))
+            }
+            imc <= 34.9 -> {
+                resultadoFinal.setText("O seu imc é de ${"%.2f".format(imc)}. Obsidade grau 1")
+                resultadoFinal.setTextColor(requireContext().getColor(R.color.yellow))
+                imagem.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.alerta))
+            }
+            imc <= 39.9 ->
+            {
+                resultadoFinal.setText("O seu imc é de ${"%.2f".format(imc)}. Obsidade grau 2")
+                resultadoFinal.setTextColor(requireContext().getColor(R.color.orange))
+                imagem.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.perigo))
+            }
+            else -> {
+                resultadoFinal.setText("O seu imc é de ${"%.2f".format(imc)}. Obsidade grau 3")
+                resultadoFinal.setTextColor(requireContext().getColor(R.color.red))
+                imagem.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.perigo))
+            }
+        }
+
+
+    }
+
 }
